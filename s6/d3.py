3889d80138734de1d69d7f5f5a43fcdf2ffb6026
RaavenBZH <emalivet56@gmail.com>
2022-04-16 13:17:45 +0200

    entrees gamertag update

--- s6/d3.py
+++ s6/d3.py
@@ -21,7 +21,7 @@ def getPointsD3() -> dict:
     ecs_finesse = Pilote("ECS Finesse", "Mercedes")
     ert_wartors = Pilote("ERT Wartors", "Mercedes")
 
-    non4me_cami = Pilote("Non4ame Cami", "RedBull")
+    non4me_cami = Pilote("Non4me Cami", "RedBull")
     soo_skyzzz = Pilote("Soo Skyzzz", "RedBull")
 
     knacki_ball = Pilote("Knacki Ball", "McLaren")
@@ -122,9 +122,9 @@ def getPointsD3() -> dict:
 
     result01.meilleurTour(rp1_gachette)
 
-
+    """
     for i in result01.getCourse():
         print(i.getGamertag(), i.getHistorique())
-
+    """
     
     return result01.getPoints()
